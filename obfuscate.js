31d3e3a2f115adca53bb6a2356b03c5c2ae82e08
Eric Jeney <me@ericjeney.com>
2012-10-20 15:05:07 -0400

    Add support for inserting invisible spans.

--- obfuscate.js
+++ obfuscate.js
@@ -1,4 +1,8 @@
 var obfuscator = {
+    randomNumber: function(max) {
+        return Math.floor((Math.random() * max) + 1);
+    },
+
     encode: function(text) {
         var newText = "";
         for(var i = 0; i < text.length; i++) {
@@ -6,9 +10,33 @@ var obfuscator = {
         }
         return newText;
     },
+
+    spans: function(text) {
+        var newText;
+        var spansInserted = 0;
+        while(spansInserted < 1 || spansInserted > 3) {
+            newText = "";
+            spansInserted = 0;
+            for(var i = 0; i < text.length; i++) {
+                newText += text[i];
+                if((!$("#encode").attr("checked") || text[i] === ';') && this.randomNumber(10) == 1) {
+                    spansInserted++;
+                    newText += "<span style=\"display:none\">";
+                    for(var r = 0; r < this.randomNumber(3)+3; r++) {
+                        newText += String.fromCharCode(this.randomNumber(26)+96);
+                    }
+                    newText += "</span>";
+                }
+            }
+        }
+
+        return newText;
+    },
+
     mailto: function(text) {
-        return "<a href=\"mailto:" + text + "\"></a>";
+        return "<a href=\"mailto:" + text + "\">" + text + "</a>";
     }
+
 };
 
 function obfuscate() {
